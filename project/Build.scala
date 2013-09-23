566659e27568232af94c7c480998db7143c77bd6
Jianfeng Tian <jianfeng.tian@gmail.com>
2013-09-23 18:03:57 +0100

    add jacoco settings

--- project/Build.scala
+++ project/Build.scala
@@ -10,19 +10,13 @@ object ApplicationBuild extends Build {
     val appVersion = "1.0-SNAPSHOT"
         
     lazy val jacoco_settings = Defaults.defaultSettings ++ Seq(jacoco.settings: _*)
- /*   
-    val appDependencies = Seq(
-        "org.mockito" % "mockito-all" % "1.9.5",
-        "org.reactivemongo" % "play2-reactivemongo_2.10" % "0.9",
-        "org.jacoco" % "org.jacoco.core" % "0.6.3.201306030806",
-        "org.jacoco" % "org.jacoco.report" % "0.6.3.201306030806")
-*/
-//    val main = play.Project(appName, appVersion, appDependencies, settings = jacoco_settings).settings(
-//        //        resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-//
-//        parallelExecution in jacoco.Config := false,
-//        jacoco.reportFormats in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8")),
-//        jacoco.includes in jacoco.Config := Seq("controllers.*", "models.*", "services.*"),
-//        jacoco.excludes in jacoco.Config := Seq("views*", "*Routes*", "controllers*routes*", "controllers*Reverse*", "controllers*javascript*", "controller*ref*", "*$$*", "*package*", "*Controller*"))
-//
+
+    val main = play.Project(appName, appVersion, Seq(), settings = jacoco_settings).settings(
+        //        resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+
+        parallelExecution in jacoco.Config := false,
+        jacoco.reportFormats in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8")),
+        jacoco.includes in jacoco.Config := Seq("controllers.*", "models.*", "services.*"),
+        jacoco.excludes in jacoco.Config := Seq("views*", "*Routes*", "controllers*routes*", "controllers*Reverse*", "controllers*javascript*", "controller*ref*", "*$$*", "*package*", "*Controller*"))
+
     }
