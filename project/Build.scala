a161d0a1b1ef2be8e1f05ee327a664d76dfb9745
Jianfeng Tian <jianfeng.tian@gmail.com>
2013-09-23 17:58:28 +0100

    add build.sbt

--- project/Build.scala
+++ project/Build.scala
@@ -16,14 +16,13 @@ object ApplicationBuild extends Build {
         "org.reactivemongo" % "play2-reactivemongo_2.10" % "0.9",
         "org.jacoco" % "org.jacoco.core" % "0.6.3.201306030806",
         "org.jacoco" % "org.jacoco.report" % "0.6.3.201306030806")
-
-    val main = play.Project(appName, appVersion, appDependencies, settings = jacoco_settings).settings(
-        //        resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-
-        parallelExecution in jacoco.Config := false,
-        jacoco.reportFormats in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8")),
-        jacoco.includes in jacoco.Config := Seq("controllers.*", "models.*", "services.*"),
-        jacoco.excludes in jacoco.Config := Seq("views*", "*Routes*", "controllers*routes*", "controllers*Reverse*", "controllers*javascript*", "controller*ref*", "*$$*", "*package*", "*Controller*"))
-
 */
+//    val main = play.Project(appName, appVersion, appDependencies, settings = jacoco_settings).settings(
+//        //        resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+//
+//        parallelExecution in jacoco.Config := false,
+//        jacoco.reportFormats in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8")),
+//        jacoco.includes in jacoco.Config := Seq("controllers.*", "models.*", "services.*"),
+//        jacoco.excludes in jacoco.Config := Seq("views*", "*Routes*", "controllers*routes*", "controllers*Reverse*", "controllers*javascript*", "controller*ref*", "*$$*", "*package*", "*Controller*"))
+//
     }
